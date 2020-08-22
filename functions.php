8492a466ef7c47f0273153476b2c6667c41198e7
Joao Freire <69178478+thatdudewiththehat@users.noreply.github.com>
2020-08-21 23:53:36 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
-//test
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
     wp_enqueue_style( 'twentytwenty', get_template_directory_uri().'/style.css' );
 }
+function google_fonts() {
+ 
+wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Raleway|Roboto&display=swap', false ); 
+}
+add_action( 'wp_enqueue_scripts', 'google_fonts' );
 ?>
